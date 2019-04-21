196f6dc2fc2e82dd57f4b7dded1992684ecf1dc8
jevticmarina89 <marina.jevtic89@gmail.com>
2019-04-21 19:06:00 +0200

    Made changes

--- src/exercises/NASACountdown.java
+++ src/exercises/NASACountdown.java
@@ -3,9 +3,17 @@ package exercises;
 /*
  * Count down a rocket launch!
  * 
- * Print the numbers from 10 to 1, then print “blastoff”. 
+ * Print the numbers from 10 to 1, then print â€œblastoffâ€�. 
  */
 
-public class NASACountdown {
+public class NASACountdown {{
 
+	
+	for(int i=10; i>=1; i--){
+		System.out.println(i);
+		
+	}
+	System.out.println("BLAST OFF!!");
+
+	}
 }
